ded44488a82fc8796d70459214762194ecd2d9de
Ian Sutherland <isutherland@azwater.gov>
2020-01-09 10:39:28 -0700

    Finished well35 lookups & changed routing in webApiConfig to allow Actions

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -18,9 +18,15 @@ namespace api
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                name: "SecondaryRoute",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
