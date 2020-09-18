dc453c02d89192af7d00c910a966895b72d1fc90
Ian Sutherland <isutherland@azwater.gov>
2020-09-18 15:41:33 -0700

    added a GET for adwr_admin.adwr_scan by id

--- Controllers/ScanController.cs
+++ Controllers/ScanController.cs
@@ -28,5 +28,19 @@ namespace api.Controllers
             return myScan;
         }
 
+        [HttpGet]
+        public Scan GetUniversalScanById(int id)
+        {
+            Util utils = new Util();
+            Scan myScan = new Scan(id);
+            string SQL = "SELECT * FROM ADWR_ADMIN.ADWR_SCAN WHERE ID = " + myScan.Id.ToString();
+            DataTable tbl = utils.GetORADataTable(SQL);
+            if (tbl.Rows.Count > 0)
+            {
+                myScan.MetaData = tbl.Rows[0][1].ToString();
+                myScan.Processed = tbl.Rows[0][1] != null ? true: false;
+            }
+            return myScan;
+        }
     }
 }
